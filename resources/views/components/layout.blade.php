b116eaac8d7c3fecd0749f4ed1e73d4c7c1d3554
AmbrozePhoebus <zoeykoolman85@gmail.com>
2026-09-07 13:45:42 +0200

    Werkende versie
    versie die werkt

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>{{ $title }}</title>
+	<style>
+		nav > a {
+			margin-right: 10px;
+			color: blue;
+		}
+	</style>
+</head>
+<body>
+
+    <nav>
+        <a href="/">Home</a>
+        <a href="/about">About Us</a>
+        <a href="/contact">Contact Us</a>
+    </nav>
+
+	<main>
+	{{ $slot }}
+	</main>
+
+</body>
+</html>
---
+++ resources/views/contact.blade.php
@@ -0,0 +1,7 @@
+<x-layout title="Contact">
+		<h1>Contact Us</h1>
+
+    <div>
+        <a href="/">Home</a>
+    </div>
+</x-layout>

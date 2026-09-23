f26bd2f02f4ca47c5bd8f69ee6c8728fbd4f8bd7
AmbrozePhoebus <143904801+AmbrozePhoebus@users.noreply.github.com>
2026-09-23 17:05:41 +0200

    added email
    added the email functions

---
+++ resources/views/emails/welcome.blade.php
@@ -0,0 +1,19 @@
+<x-layout title="Welcome to Space Cards">
+
+    <div class="py-10">
+
+        <h1 class="text-3xl font-bold text-[#F4FBFF] mb-4">
+            Welcome to Space Cards!
+        </h1>
+
+        <p class="text-[#D9F4FF]/80 mb-4">
+            Thanks for joining the Space Cards database.
+        </p>
+
+        <p class="text-[#D9F4FF]/80">
+            Your account is ready. Start discovering cards and build your collection!
+        </p>
+
+    </div>
+
+</x-layout>
